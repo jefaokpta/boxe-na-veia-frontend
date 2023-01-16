b2595af5901ccb4c6cbdef3d952ae14f01b341d3
jefaokpta <jefaokpta@hotmail.com>
2023-01-16 13:53:52 -0300

    seu burro precisava dos modulos, Button e Input pra funcionar

--- src/app/boxe/pages/home/home.component.ts
+++ src/app/boxe/pages/home/home.component.ts
@@ -1,17 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ServerService} from "../../../service/server.service";
-import {Observable} from "rxjs";
+import {finalize, Observable, take} from "rxjs";
+import {Boxer} from "../../../model/boxer";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  boxers: Observable<any> = this.server.getBoxers();
+  // boxers: Observable<any> | undefined;
+  boxers = this.server.getBoxers()
+    .pipe(
+      finalize(() => this.loading = false)
+    );
+  loading = true;
 
   constructor(private server: ServerService) { }
+
+  ngOnInit(): void {
+    // this.boxers = this.server.getBoxers()
+    //   .pipe(
+    //     finalize(() => this.loading = false)
+    //   );
+  }
 
 
 }
--- src/app/service/server.service.ts
+++ src/app/service/server.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Boxer} from "../model/boxer";
+import {delay, map, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,11 @@ export class ServerService {
   constructor(private http: HttpClient) { }
 
   getBoxers(){
-    return this.http.get('http://localhost:8080/api/boxers')
+    return this.http.get<Boxer[]>('http://localhost:8080/api/boxers')
+      .pipe(
+        delay(3000),
+        map(boxers => boxers.filter(boxer => boxer.id < 4)),
+        // tap(boxers => console.log(boxers))
+      );
   }
 }
